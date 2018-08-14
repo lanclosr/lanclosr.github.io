223eff25eed84287bd29bfcd33682117e1b6e078
Ryan Lanclos <rlanclos@gmail.com>
2018-08-13 19:49:54 -0700

    minor  changes on notes
    notes for code

--- 575/lab2/2.2/js/main.js
+++ 575/lab2/2.2/js/main.js
@@ -1,34 +1,36 @@
 // setup the map
 window.onload = function(){
+    
+    //set the window size variables for the element
     var width = 900,
         height = 600;
-
+    //svg setup
     var svg = d3.select( "body" )
         .append( "svg" )
         .attr( "width", width )
         .attr( "height", height )
         .attr("class", "svg");
-
+    //variable to support calls later
     var g = svg.append( "g" );
-
+    //projection for the map
     var albersProjection = d3.geoAlbers()
         .rotate([95.4, 0])
         .center([0, 29.9])
         .scale(35000)
         .translate([width / 2, height / 2]);
-
+    //set the path for the map
     var geoPath = d3.geoPath()
         .projection( albersProjection );
-
+    //load the data
     d3.csv("data/watershedAttributes.csv", function(attributeData){
         console.log(attributeData);
     })
 
     d3.json("data/watershedstopo.json", function(watershedData){
-
+    //covert topojson to geojson
     var geojson = topojson.feature(watershedData, watershedData.objects.watersheds4326)
     console.log(geojson);
-
+    //draw the map 
     var watersheds = g.selectAll( "path" )
         .data( geojson.features )
         .enter()
