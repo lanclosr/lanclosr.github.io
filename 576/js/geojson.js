c3f19884c57534d02b31711d39a569ef359d6d5b
Ryan Lanclos <rlanclos@gmail.com>
2018-12-05 00:36:50 -0600

    FIX ME: Popup on geolocation

--- 576/js/geojson.js
+++ 576/js/geojson.js
@@ -96,6 +96,6 @@ $(document).ready(function() {
     function onLocationFound(e) {
         console.log(e); 
         L.marker(e.latlng).addTo(map)
-            .bindPopup('THIS AREA IS DESIGNATED AS' + {ZONE_SUBTY}).openPopup();
+            .bindPopup('THIS AREA IS DESIGNATED AS **add attribute here**').openPopup();
     }
 });
