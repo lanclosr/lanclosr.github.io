31eeb98062fce11b229ba1d9edb07d7972ff918f
Ryan Lanclos <rlanclos@gmail.com>
2018-12-05 13:22:54 -0600

    syntax

--- 576/js/geojson.js
+++ 576/js/geojson.js
@@ -96,7 +96,7 @@ $(document).ready(function() {
     function onLocationFound(e) {
         console.log(e); 
         L.marker(e.latlng).addTo(map)
-        .bindPopup(femaZones.{ZONE_SUBTY}).openPopup()
-            //.bindPopup('THIS AREA IS DESIGNATED AS **add attribute here**').openPopup();
+        .bindPopup(femaZones.ZONE_SUBTY).openPopup()
     }
+            //.bindPopup('THIS AREA IS DESIGNATED AS **add attribute here**').openPopup();
 });
