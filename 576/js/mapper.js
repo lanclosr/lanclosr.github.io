eb269746f90074409213f37836328388e27b2bad
Ryan Lanclos <rlanclos@gmail.com>
2018-12-06 23:58:53 -0600

    test esri

---
+++ 576/js/mapper.js
@@ -0,0 +1,15 @@
+require([
+    "esri/Map",
+    "esri/views/MapView"
+], function(Map, MapView) {
+    var map = new Map({
+        basemap: "streets"
+    });
+
+    var view = new MapView({
+        container: "viewDiv",
+        map: map,
+        zoom: 4,
+        center: [15, 65] // longitude, latitude
+    });
+});
