ddbbf204498cfb15117f78c454a478c5cf403368
Ryan Lanclos <rlanclos@gmail.com>
2018-12-06 22:25:37 -0600

    popups

--- 576/js/geojson.js
+++ 576/js/geojson.js
@@ -2,20 +2,31 @@
 //leaflet map for main index.html page
 $(document).ready(function() {
    navigator.geolocation.getCurrentPosition(function(location) {
+       
        //capture lat and lng of the user location
        var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+       
        //create the map using the lat and long as center
        var map = L.map('map').setView(latlng, 13);
+       
        //set the basemap
        var basemap = L.esri.basemapLayer('Streets').addTo(map);
+       
+       //add the hazard layer
        var hazards = L.esri.featureLayer({
            url: 'https://services.arcgis.com/0ZRg6WRC7mxSLyKX/arcgis/rest/services/Hazard_Layer_Symbolized/FeatureServer/0',
            maxzoom: 13,
-       }).addTo(map); 
-       //drop a marker at the user location from lat and long
-       var marker = L.marker(latlng).bindPopup(function (layer) {
-           return L.Util.template('<p>THIS AREA IS DESIGNATED AS {ZONE_SUBTY}.</p>', layer.feature.properties);
        }).addTo(map);
+       
+       //bind a popup to the features
+       var showMe = hazards.bindPopup(function (layer) {
+           return L.Util.template('<p>THIS AREA IS DESIGNATED AS {ZONE_SUBTY}.</p>', layer.feature.properties);
+       });
+       
+       //drop a marker at the user location from lat and long
+       var marker = L.marker(latlng)
+       }).addTo(map);
+    
        //add Esri geocoder
         var arcgisOnline = L.esri.Geocoding.arcgisOnlineProvider();
 
