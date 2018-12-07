fa2dda0cdd2ec7567063651a39c1184ff00271f2
Ryan Lanclos <rlanclos@gmail.com>
2018-12-06 19:25:38 -0600

    style

--- 576/js/geojson.js
+++ 576/js/geojson.js
@@ -8,13 +8,20 @@ $(document).ready(function() {
        var map = L.map('map').setView(latlng, 14);
        //set the basemap
        var basemap = L.esri.basemapLayer('Streets').addTo(map);
-       //load FEMA national layer
+       //set style of for the flood data
+       var floodplainStyle = {
+           "color": "DarkSlateBlue",
+           "weight": 0.5,
+           "opacity": 0.4
+       };
+       //load FEMA national layer and use the style above
        var femaZones = L.esri.featureLayer({
            url: 'https://hazards.fema.gov/gis/nfhl/rest/services/public/NFHL/MapServer/28',
            maxzoom: 14,
-           style: watersheds,
+           style: floodplainStyle,
        }).addTo(map); 
        //drop a marker at the user location from lat and long
        var marker = L.marker(latlng).addTo(map);
+       
    });
 });
