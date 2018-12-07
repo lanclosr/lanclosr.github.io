085aa29acf5347fb7dea07e2ba55f5fccc7428b8
Ryan Lanclos <rlanclos@gmail.com>
2018-12-06 19:02:21 -0600

    simple

--- 576/js/geojson.js
+++ 576/js/geojson.js
@@ -1,103 +1,12 @@
 //hattip to leaflet tutorials!!
 //leaflet map for main index.html page
 $(document).ready(function() {
-   //find location
-    navigator.geolocation.getCurrentPosition(function(location) {
-        var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
-    
-    //set variables for the map
-    var light = L.esri.basemapLayer('Gray'),
-        streets   = L.esri.basemapLayer('Streets'),
-        topographic   = L.esri.basemapLayer('Topographic');
-	//set baseMaps group variable to add to the layer control
-    var baseMaps = {
-        "Light Gray": light,
-        "Street Map": streets,
-        "Topographic": topographic
-    };
-    //set the map view!
-    var map = L.map('map', {
-        scrollWheelZoom: false,
-        setView: latlng,
-        zoom: 10,
-        layers: [topographic]
-    });
-    
-    var marker = L.marker(latlng).addTo(map);
-        
-    //create pane to draw lables
-    map.createPane('canvas');
-    map.getPane('canvas').style.zIndex = 200;
-    map.getPane('canvas').style.pointerEvents = 'none';
-    
-    //add layer control to the map 
-    L.control.layers(baseMaps).addTo(map);
-    
-    //add Navbar to the map
-    L.control.navbar().addTo(map);
-    
-    //add Esri geocoder
-    var arcgisOnline = L.esri.Geocoding.arcgisOnlineProvider();
-
-    var searchControl = L.esri.Geocoding.geosearch({
-        providers: [
-            arcgisOnline,
-            L.esri.Geocoding.mapServiceProvider({
-                label: 'States and Counties',
-                url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer',
-            })
-        ]
-    }).addTo(map);
-    
-    //set the style for floodplain
-    var floodplainStyle = {
-        "color": "DarkSlateBlue",
-        "weight": 0.5,
-        "opacity": 0.4
-    };
-    
-    //load FEMA national layer
-    var femaZones = L.esri.featureLayer({
-        url: 'https://hazards.fema.gov/gis/nfhl/rest/services/public/NFHL/MapServer/28',
-        minZoom:15,
-        style: floodplainStyle,
-    }).addTo(map);
-    
-    //set popup on the FEMA layer when people click manually
-    femaZones.bindPopup(function (layer) {
-        return L.Util.template('<p>THIS AREA IS DESIGNATED AS {ZONE_SUBTY}.</p>', layer.feature.properties);
-       });
-    
-    //add a legend to the bottom right
-    var legend = L.control({position: 'bottomright'});
-
-    legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-        classes = ["FEMA 0.2% Flood Zone "],
-        labels = ["img/flood.png"];
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < classes.length; i++) {
-        div.innerHTML +=
-            ("<img src="+ labels[i] +" height='20' width='20'>  ") + classes[i] +'<br>';
-    }
-
-    return div;
-    };
-
-    legend.addTo(map);
-    
-    
-    //set geolocation for the user
-    //$('.pure-button').on('click', function(){
-        //map.locate({setView: true, maxZoom: 15});
-    //});
-
-    //map.on('locationfound', onLocationFound);
-    //function onLocationFound(e) {
-        //console.log(e);
-        //L.marker(e.latlng).addTo(map)
-    //}
-    });
+   navigator.geolocation.getCurrentPosition(function(location) {
+       var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+       var mymap = L.map('mapid').setView(latlng, 13)
+       
+       L.esri.basemapLayer('Streets').addTo(map);
+       
+       var marker = L.marker(latlng).addTo(mymap);
+   });
 });
